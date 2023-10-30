ad58ad25b16aa010c7973e172941846352e45641
Amena Akter <143479387+jrtamena@users.noreply.github.com>
2023-10-30 22:04:02 +0600

    Delete main-project-with-pweb directory

--- main-project-with-pweb/application/boot/boot/form/member_form.py
+++
@@ -1,9 +0,0 @@
-from pweb import PWebForm, fields
-
-
-class MemberForm(PWebForm):
-    firstname = fields.String(required=True, error_messages={"required": "Please enter first name"})
-    lastname = fields.String(allow_none=True)
-    email = fields.Email(required=True, error_messages={"required": "Please enter email"})
-    password = fields.String(required=True, error_messages={"required": "Please enter password"})
-    dob = fields.String(required=True, error_messages={"required": "Please enter date of birth"})
--- main-project-with-pweb/application/config/app_config.py
+++
@@ -1,6 +0,0 @@
-from pweb import PWebAppConfig
-
-
-class Config(PWebAppConfig):
-    APP_NAME = "main-project-with-pweb"
-    PORT: int = 1212
